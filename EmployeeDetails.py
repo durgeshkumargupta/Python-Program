6e5894ad60c8dc2a9c4ce6e925f68ff3687d3661
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-02-21 20:43:26 +0530

    Update EmployeeDetails.py

--- EmployeeDetails.py
+++ EmployeeDetails.py
@@ -5,7 +5,7 @@ for i in range(1,n+1):
     temp=dict()
     emp_no=input("Enter Employee Number:")
     temp['name']=input("Enter employee name:")
-    temp["basic-salary"]=input("Entre basic salary:")
+    temp["basic-salary"]=input("Enter basic salary:")
     temp['hra']=input("Enter HRA:")
     temp['salary']=temp['basic-salary']+temp['hra']
     d[emp_no]=temp
