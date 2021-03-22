07836b9f6d6701b6a2b231e63c99d31bf25acb4a
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-03-22 20:12:05 +0530

    Update P1.py

--- Program-8/P1.py
+++ Program-8/P1.py
@@ -1,20 +1,46 @@
-try:
-    n1=int(input("Enter First Number:"))
-    n2=int(input("Enter Second Number:"))
-    c2=n1/n2
+while True:
+    print("1.Check Value Error")
+    print("2.Check File Not Found Error")
+    print("3.Check Type Error")
+    print("4.Check IOError")
+    print("5.Check File Exist Error")
+    print("0.Exit")
+    n=int(input("Enter Choice:"))
+    if n==1:
+        try:
+            f = open('abc.txt', 'r')
+            print("Successfully")
+        except ValueError:
+            print("Value Error")
+    elif n==2:
+        try:
+            f = open('abc2.txt', 'r')
+            print("Successfully")
+        except FileNotFoundError:
+            print("File Not Found error")
+    elif n==3:
+        try:
+            f = open('abc.txt', 'r', 'w')
+            print("Successfully")
+        except TypeError:
+            print("Type Error")
+    elif n==4:
+        try:
+            f = open('abc.txt', 'w+')
+            f.write("Sample")
+            f1 = open('ccc.txt', 'r')
+            print("Successfully")
+        except IOError:
+            print("IO Error")
+    elif n==5:
+        try:
+            f=open('abc2.txt','w')
+            print("Successfully")
+        except FileExistsError:
+            print("File Exist Error")
+    elif n==0:
+        break
+    else:
+        print("Invalid input Please try again ")
 
-except ValueError:
-    print("Value Error")
-except ZeroDivisionError:
-    print("Zero Divisible Error")
-except NameError:
-    print("Name Error")
-except KeyboardInterrupt:
-    print("Keyboard Interrupted Error")
-except TypeError:
-    print("Type Error")
-except Exception:
-    print("Other type of Exception")
-else:
-    print("Successful")
 
