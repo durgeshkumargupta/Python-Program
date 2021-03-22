9eba3ff854eb990f3006ae61a091ff92456318c5
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-03-22 19:34:23 +0530

    Update P1.py

--- Program-7/P1.py
+++ Program-7/P1.py
@@ -1,4 +1,4 @@
-# Method overriding
+# Method overriding N Number of Employee
 class Employee:
     raise_amt=1.04
     def __init__(self,first,last,empid,pay):
@@ -6,7 +6,7 @@ class Employee:
         self.last=last
         self.empid=empid
         self.pay=pay
-    def apply_raise(self):
+    def apply_raise(self,raise_amt):
         self.pay=self.pay*self.raise_amt
     def display(self):
         print("First Name:",self.first)
@@ -14,21 +14,42 @@ class Employee:
         print("Employee ID:",self.empid)
         print("Pay:",self.pay)
 class Developer(Employee):
-    raise_amt = 1.05
-
-    def apply_raise(self):
+    def apply_raise(self,raise_amt):
         self.pay = self.pay * self.raise_amt
 class Manager(Employee):
-    raise_amt = 1.06
-
-    def apply_raise(self):
+    def apply_raise(self,raise_amt):
         self.pay = self.pay * self.raise_amt
 
-emp1=Manager("Durgesh","Kumar","RVCE20MCA064",50000)
+man={}
+dev={}
+n=int(input("Enter Number of Employee:"))
+for i in range(1,n+1):
+    print("1.Enter Manager Data:")
+    print("2.Enter Developer Data:")
+    print("3.Exit")
+    ch=int(input("Enter your choice:"))
+    if ch==1:
+        id=input("Enter employee ID:")
+        first=input("Enter First Name:")
+        last=input("Enter last Name:")
+        raise_amt=int(input("Enter Raise amount:"))
+        pay=int(input("Enter Pay Amount:"))
+        e1=Employee(first,last,id,pay)
+        e1.apply_raise(raise_amt)
+        man[i]=e1.__dict__
+    elif ch==2:
+        id = input("Enter employee ID:")
+        first = input("Enter First Name:")
+        last = input("Enter last Name:")
+        raise_amt=int(input("Enter Raise amount:"))
+        pay = int(input("Enter Pay Amount:"))
+        e2 = Employee(first, last, id, pay)
+        e2.apply_raise(raise_amt)
+        man[i] = e2.__dict__
+    elif ch==3:
+        break
+    else:
+        print("Invalid Input please try again")
 
-emp2=Developer("Prince","Kumar","RVCE20MCA065",60000)
-
-emp1.apply_raise()
-emp2.apply_raise()
-print(emp1.display())
-print(emp2.display())
+print(man)
+print(dev)
