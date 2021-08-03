0ea815d914a414fdaee8422649f272dbd83937d2
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-08-03 19:48:48 +0530

    Update P1.py

--- Program-7/P1.py
+++ Program-7/P1.py
@@ -1,49 +1,47 @@
 # Method overriding N Number of Employee
 class Employee:
-    def __init__(self,first,last,empid,pay):
-        self.first=first
-        self.last=last
-        self.empid=empid
-        self.pay=pay
-    def apply_raise(self,raise_amt):
-        self.pay=self.pay*raise_amt
+    def setData(self):
+        self.first=input("Enter First Name:")
+        self.last=input("Enter Last Name:")
+        self.empid=input("Enter ID:")
+        self.pay=input("Enter Pay Amount")
+    def apply_raise(self):
+        self.raise_amt = int(input("Enter Raise amount:"))
+        self.pay = int(input("Enter Pay Amount:"))
+        self.pay=self.pay*self.raise_amt
     def display(self):
         print("First Name:",self.first)
         print("Last Name:",self.last)
         print("Employee ID:",self.empid)
         print("Pay:",self.pay)
 class Developer(Employee):
-    def apply_raise(self,raise_amt):
-        self.pay = self.pay * raise_amt
+    def apply_raise(self):
+        self.raise_amt = int(input("Enter Raise amount:"))
+        self.pay = int(input("Enter Pay Amount:"))
+        self.pay = self.pay * self.raise_amt
 class Manager(Employee):
-    def apply_raise(self,raise_amt):
-        self.pay = self.pay * raise_amt
+    def apply_raise(self):
+        self.raise_amt = int(input("Enter Raise amount:"))
+        self.pay = int(input("Enter Pay Amount:"))
+        self.pay = self.pay * self.raise_amt
 
 man={}
 dev={}
 n=int(input("Enter Number of Employee:"))
-for i in range(1,n+1):
+for i in range(n):
     print("1.Enter Manager Data:")
     print("2.Enter Developer Data:")
     print("3.Exit")
     ch=int(input("Enter your choice:"))
     if ch==1:
-        id=input("Enter employee ID:")
-        first=input("Enter First Name:")
-        last=input("Enter last Name:")
-        raise_amt=int(input("Enter Raise amount:"))
-        pay=int(input("Enter Pay Amount:"))
-        e1=Manager(first,last,id,pay)
-        e1.apply_raise(raise_amt)
+        e1=Manager()
+        e1.setData()
+        e1.apply_raise()
         man[i]=e1.__dict__
     elif ch==2:
-        id = input("Enter employee ID:")
-        first = input("Enter First Name:")
-        last = input("Enter last Name:")
-        raise_amt=int(input("Enter Raise amount:"))
-        pay = int(input("Enter Pay Amount:"))
-        e2 = Developer(first, last, id, pay)
-        e2.apply_raise(raise_amt)
+        e2 = Developer()
+        e2.setData()
+        e2.apply_raise()
         dev[i] = e2.__dict__
     elif ch==3:
         break
