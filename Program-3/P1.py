9737a88f04efe51d981801b22265e2a2780f46c4
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-08-03 23:42:33 +0530

    Update P1.py

--- Program-3/P1.py
+++ Program-3/P1.py
@@ -11,11 +11,6 @@ class employee():
         self.da = 0.25 * self.basic
         self.gross_sal = self.basic + self.hra + self.da
         self.net_sal = self.basic - self.ded
-        self.update_emp()
-
-    def update_emp(self):
-        d.update({self.name: {"basic": self.basic, "hra": self.hra, "da": self.da, "gross_sal": self.gross_sal,
-                              "net_sal": self.net_sal}})
 
     def search_emp(self, name):
         flag = 0
@@ -32,18 +27,17 @@ class employee():
         for key in d:
             print(key, d[key])
 
-
-class emp1(employee):
-    em = employee()
-    while True:
+while True:
         print("1.Add employee")
         print("2.Search an employee")
         print("3.Print employee details")
         print("4.Update employee details")
         print("5.EXIT")
+        em = employee()
         ch = int(input("enter your choice:"))
         if ch == 1:
             em.add_emp()
+            d[em.name]=em.__dict__
         elif ch == 2:
             name = input("enter employee name to search:")
             em.search_emp(name)
@@ -52,7 +46,9 @@ class emp1(employee):
         elif ch == 4:
             name = input("enter employee name to update:")
             em.add_emp()
-            em.update_emp()
+            for key in d:
+                if key==name:
+                    d[em.name]=em.__dict__
         elif ch==5:
             break
         else:
